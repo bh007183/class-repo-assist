1e054152e1d7647106ac6f1d0a20d631b36f7062
Ben Hopkins <bjhops17@gmail.com>
2022-12-23 15:38:54 -0800

    stopping point for day

--- script.js
+++ script.js
@@ -2,39 +2,48 @@ const fs = require("fs");
 const path = require("path");
 const initialpath =
   "/Users/benhopkins/UWBootCamp/instructorrepo/fullstack-online/01-Class-Content";
-// async function Modules(path){
 
-//     const folders = await fs.readdirSync(path)
-
-//     folders.forEach(folder => {
-//         if(folder != "solved" || folder != "main"){
-//            Modules(path + "/" + folder)
-//         }
-//     })
-
-// }
-
-var copyRecursiveSync = function (src, dest) {
-  
+function copyRecursiveSync(src, dest) {
   var exists = fs.existsSync(src);
   var stats = exists && fs.statSync(src);
   var isDirectory = exists && stats.isDirectory();
-
+  
   if (isDirectory) {
     fs.mkdirSync(dest);
     fs.readdirSync(src)
-    .filter(name => name !== "Solved" && name !== "Main" && name !== "main" && name !== "solved")
-    .forEach(function (childItemName) {
+      .filter(
+        (name) =>
+          name !== "Solved" &&
+          name !== "Main" &&
+          name !== "main" &&
+          name !== "solved"
+      )
+      .forEach(function (childItemName) {
+        // if(src.split("/")[8].includes(getLatestWeek('/Users/benhopkins/UWBootCamp/studentrepo/UW-VIRT-FSF-PT-12-2022-U-LOLC')))
         copyRecursiveSync(
           path.join(src, childItemName),
           path.join(dest, childItemName)
         );
-    });
+      });
   } else {
     fs.copyFileSync(src, dest);
   }
-};
+}
 copyRecursiveSync(
   "/Users/benhopkins/UWBootCamp/instructorrepo/fullstack-ground/01-Class-Content",
   "./otter"
 );
+
+function getLatestWeek(studentRepo) {
+  var exists = fs.existsSync(studentRepo);
+  var stats = exists && fs.statSync(studentRepo);
+  var isDirectory = exists && stats.isDirectory();
+
+  if (isDirectory) {
+    let folders = fs
+      .readdirSync(studentRepo)
+      .filter((childItemName) => childItemName.match(/\d/));
+    let lastWeekInStudentRepo = folders[folders.length - 1].split("-")[0];
+    return lastWeekInStudentRepo;
+  }
+}
