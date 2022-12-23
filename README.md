e8cf1d2dc524192079da4184290d654a02cddb2e
Ben Hopkins <bjhops17@gmail.com>
2022-12-23 12:46:42 -0800

    added object generator for daily info

---
+++ README.md
@@ -0,0 +1,7 @@
+* This is a project that uses the repo and class repository to push up class files. Note that it uses the lesson plans readme to break it down into days and there is a typo in week 21 day 3 where it says:
+
+* In this lesson, students will complete activities `11-Ins_Sign-JWT` through `28-Stu_Mini-Project`.
+
+* That should read: * In this lesson, students will complete activities `21-Ins_Sign-JWT` through `28-Stu_Mini-Project`.
+
+* my work arround right now is just to modify that typo manually.
--- lesson-plan.js
+++ lesson-plan.js
@@ -1,13 +1,12 @@
 ///Users/benhopkins/UWBootCamp/instructorrepo/fullstack-ground/02-Lesson-Plans/Part-Time
 const fs = require("fs");
 const path = require("path");
-var copyRecursiveSync = function (src, dest) {
+let arr = []
+var DailyLessonObjGenerator = function (src, dest) {
   
     var exists = fs.existsSync(src);
     var stats = exists && fs.statSync(src);
     var isDirectory = exists && stats.isDirectory();
-    let arr = []
-  
     if (isDirectory) {
       fs.mkdirSync(dest);
       fs.readdirSync(src)
@@ -18,18 +17,44 @@ var copyRecursiveSync = function (src, dest) {
           );
       });
     } else {
-      if(!src.includes("README")){
+      if(src.includes("LESSON")){
+        // Gets week folder name
         let grandparent = src.split("/")[8]
+        // Gets day folder name
         let parent = src.split("/")[9]
+        // reads file
         let data = fs.readFileSync(src, "utf-8")
+
+        // captures sentance that tells us what lessons belong to what days
+        let reg = new RegExp("(In this lesson, students will complete activities .*through.*`)")
+        let words = data.split(reg)
+         
+        // captures first lesson number of day
+        let start  = new String(words[1]).match(/\d/g)
+        if(start){
+           start = start.join("").slice(0,2)
+        } 
+        // captures last lesson number of day
+        let end  = new String(words[1]).match(/\d/g) 
+        if(end){
+          end = end.join("").slice(2)
+        }
+
+    
+        // creates array with all lesson numbers for that day
+        let lessons = []
+      
+        for(let i = Number(start); i<= Number(end); i++){
+           lessons.push(i)
+        }
         
-            let words = data.split(/[\s\/\`\[\]\(\)]/)
-            let days = words.filter(data => data.includes("-Ins_") || data.includes("-Stu_"))
-            let lessons = [...new Set(days)];
+        
+       
+            //object containing all leson numbers, day, and week data
           let obj = {
             grandparent,
             parent,
-            lessons: lessons.sort()
+            lessons: lessons
           }
           arr.push(obj)
 
@@ -37,10 +62,12 @@ var copyRecursiveSync = function (src, dest) {
       }
      
     }
-    console.log(arr)
+    
+
   };
-  copyRecursiveSync(
+  DailyLessonObjGenerator(
     "/Users/benhopkins/UWBootCamp/instructorrepo/fullstack-ground/02-Lesson-Plans/Part-Time",
     "./otter"
   );
+  console.log(arr)
   
