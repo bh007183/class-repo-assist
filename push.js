0abf4862fba9b44fb5017f829a3cbc847ad6fc18
Ben Hopkins <bjhops17@gmail.com>
2023-01-05 22:10:38 -0800

    pushed folders

--- push.js
+++ push.js
@@ -3,7 +3,7 @@ async function command(){
     try{
         await exec("cd ~/UWBootCamp/studentrepo/");
         await exec("git add .");
-        await exec("git commit -m 'pushed solved folders'");
+        await exec("git commit -m 'pushed folders'");
         await exec("git push");
     }catch(err){
         console.log("error")
