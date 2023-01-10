5352c168a8cf1495b370244821745b263d604c8c
Ben Hopkins <bjhops17@gmail.com>
2023-01-09 21:31:52 -0800

    added proper file path for push

--- push.js
+++ push.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 async function command(){
     try{
-        await exec("cd ~/UWBootCamp/studentrepo/");
+        await exec("cd ~/UWBootCamp/studentrepo/UW-VIRT-FSF-PT-12-2022-U-LOLC");
         await exec("git add .");
         await exec("git commit -m 'pushed folders'");
         await exec("git push");
